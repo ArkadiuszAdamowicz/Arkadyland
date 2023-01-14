f5ad1cd7c009fa6f9092c60e05c39473fdab0cca
Arek <arkadiusz.adamowicz.1995@gmail.com>
2023-01-14 20:02:20 +0100

    first version

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+const cookieBox = document.querySelector('.cookie-box')
+const cookieBtn = document.querySelector('.cookie-btn')
+const footerYear = document.querySelector('.footerYear')
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const nav = document.querySelector('.navbar')
+    const allNavItems = document.querySelectorAll('.nav-link')
+    const navList = document.querySelector('.navbar-collapse')
+    
+    function addShadow() {
+    if (window.scrollY >= 300) {
+        nav.classList.add('shadow-bg')
+    } else {
+        nav.classList.remove('shadow-bg')
+    }
+    }
+    
+    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+    
+    window.addEventListener('scroll', addShadow)
+    })
+
+
+    const showCookie = () => {
+        const cookieEaten = localStorage.getItem('cookie')
+        
+        if (cookieEaten) {
+            cookieBox.classList.add('hide')
+        }
+    }
+    
+    const handleCookieBox = () => {
+        localStorage.setItem('cookie', 'true')
+        cookieBox.classList.add('hide')
+    } 
+    
+    cookieBtn.addEventListener('click', handleCookieBox)
+    showCookie()
+
+    const handleCurrentYear = () => {
+        const year = (new Date).getFullYear();
+        footerYear.innerText = year;
+    }
+
+    handleCurrentYear();
+
+ 
